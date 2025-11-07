11e31a1f623781d3ea15804a10fe64a94b996acf
Yudistira <126404723+yudistira1235@users.noreply.github.com>
2025-11-07 19:00:14 +0700

    LuasLingkaran.php

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,14 +1,28 @@
 <?php
 
+namespace App\Math;
+
 class LuasLingkaran {
 
     public const phi = 3.14;
-    public int $jari;
+    protected int $jari;
+    
+    public function __construct($isiJari = 1) {
+        $this->jari = $isiJari; //10
+    }
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "ini dari static";
+    }
+
+    public function __destruct() {
+        echo "<br/>";
+        echo "udah ah cape";
+    }
 }
-
-$Lingkaran = new LuasLingkaran();
-$Lingkaran->jari = 7;
-
-$rumus = LuasLingkaran::phi * $Lingkaran->jari * $Lingkaran->jari;
-
-echo "Hasilnya adalah: ". $rumus;
