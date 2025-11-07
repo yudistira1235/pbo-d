ee32c79f20e64dfaa3c9e8701287d9ed4582a615
Yudistira <126404723+yudistira1235@users.noreply.github.com>
2025-11-07 19:02:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,1 @@
-<?php
 
-require_once('./LuasLingkaran.php'); //panggil file LuasLingkaran.php
-
-use App\Math\LuasLingkaran; //panggil namespace
-
-$Lingkaran = new LuasLingkaran(10);
-$Lingkaran->tampil('roda'); //panggil method
-LuasLingkaran::testing();
